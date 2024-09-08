b2f5a1a8990f3a9738be94d8e878a63ec74fcba3
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-08 23:38:40 +0530

    code modified

---
+++ script.js
@@ -0,0 +1,8 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temprature = document.querySelector(".temprature");
+const description = document.querySelector(".description");
+
+const humidty = document.getElementById("humidity");
+const wind_speed = document.getElementById("wind-speed");
