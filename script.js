b06d6c9bfa4e6b9aa6ea73d4d50b75dc30894227
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-08 23:54:33 +0530

    code modified

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const description = document.querySelector(".description");
 
 const humidty = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
+
+async function checkWeather(city) {
+  const api_key = "dc77db53f4748aeedcb384b694b2806c";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const weather_data = await fetch(`${url}`).then((response) =>
+    response.json()
+  );
+
+  console.log(weather_data);
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
