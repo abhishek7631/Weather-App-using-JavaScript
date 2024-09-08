c25fac6aeb66bfecb21a33f53f752e52d621d834
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-09 00:37:35 +0530

    code modified

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const description = document.querySelector(".description");
 const humidty = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
 
+const location_not_found = document.querySelector(".location-not-found");
+
+const weather_body = document.querySelector(".weather-body");
+
 async function checkWeather(city) {
   const api_key = "dc77db53f4748aeedcb384b694b2806c";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
@@ -14,6 +18,14 @@ async function checkWeather(city) {
   const weather_data = await fetch(`${url}`).then((response) =>
     response.json()
   );
+
+  if (weather_data.cod === `404`) {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    return;
+  }
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
 
   temprature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
 
